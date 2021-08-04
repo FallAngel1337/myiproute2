77ae57ffec1948ab1df309350a5dae804be585f9
FallAngel1337 <fallangel@protonmail.com>
2021-08-04 14:51:34 -0300

    ifctl: added some basic stuff

--- src/include/ifconfig/ifctl.c
+++ src/include/ifconfig/ifctl.c
@@ -4,3 +4,33 @@
  * 
  * ifconfig implementation
 */
+
+#include "ifctl.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#include <sys/socket.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+static const struct option long_options[] = {
+    {"verbose", no_argument, NULL, 'v'},
+    {"help", no_argument, NULL, 'h'},
+    {"version", no_argument, NULL, 's'}
+};
+
+static uint8_t _flags = 0;
+
+static __attribute__((noreturn)) void usage(void)
+{
+    // show help here
+    exit(-EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) usage();
+    return 0;    
+}
--- src/include/ifconfig/ifctl.h
+++ src/include/ifconfig/ifctl.h
@@ -8,3 +8,19 @@
  * that will be used to control/interact with the
  * linux network interfaces.
 */
+
+#ifndef _IFCTL_H
+#define _IFCTL_H
+
+#ifdef __linux__
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <linux/netlink.h>
+
+#else
+#error ** Seems your not at a GNU/Linux environment ... **
+
+#endif  /* __linux__ */
+
+#endif /* _IFCTL_H */
