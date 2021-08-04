42ce3220c9dcff90a523320f3a1e11500abf743f
FallAngel1337 <fallangel@protonmail.com>
2021-08-04 15:23:58 -0300

    ifctl.h: basic structure

--- src/include/ifconfig/ifctl.h
+++ src/include/ifconfig/ifctl.h
@@ -17,10 +17,50 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <linux/netlink.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+// some basic flags to use
+enum {
+    CHECK_UP = 0x1, /* if interface is up */
+    CHECK_DOWN = 0x2, /* if interface is down */
+    SET_UP = 0x4, /* set interface up */
+    SET_DOWN = 0x8, /* set interace down */
+
+    IGNORE_LOOPBACK = 0x10, /* ignore the loopback interface */
+    IF_ALL = 0x20, /* retrieve information about all found interfaces */
+};
+
+struct if_info {
+    struct sockaddr if_addr;
+    union
+    {
+        struct sockaddr if_netmask;
+        struct sockaddr if_wildcard;
+    } if_mask;
+
+    union 
+    {
+        struct sockaddr if_broadcast; /* broadcast address */
+        struct sockaddr if_network; /* network address (first (not usable) address) */
+    };
+};
+#define if_netmask if_mask.if_netmask
+#define if_wilcard if_mask.if_wildcard
+
+struct if_list {
+    char **if_name;
+    size_t if_len; /* number of interfaces */
+};
+
+// return the name of founding interfaces e.g.: eth0
+struct if_list* get_if_list(struct if_list *ifl, uint8_t _flags);
+
+// return information about the interface
+struct if_info* get_if_info(struct if_info *ifi, uint8_t _flags);
 
 #else
 #error ** Seems your not at a GNU/Linux environment ... **
 
 #endif  /* __linux__ */
-
 #endif /* _IFCTL_H */
